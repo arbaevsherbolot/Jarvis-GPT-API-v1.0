d014db974ee95b5dd3d06f153d3d6988ee51dba3
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-10-23 12:14:19 +0600

    Setup valid pipes

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function start() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT || 888;
 
+  //Set CORS configuration
   app.enableCors();
 
   //Set the global prefix for our server
   app.setGlobalPrefix('api');
+
+  //Set the global validation pipes for our server
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port, () =>
     console.log(`📢 Server starting on: http://localhost:${port}/ ⚡️`),
