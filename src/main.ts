b37efc056d03ca016b5266f3fb332a921677467b
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-11-12 05:57:48 +0600

    Temperly removed Google OAUTH

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 
 async function start() {
   const app = await NestFactory.create(AppModule, { cors: true });
