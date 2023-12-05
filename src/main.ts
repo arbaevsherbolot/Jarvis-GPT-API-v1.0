88c8e8da545a3c67e8b3aaa6ca7b3d2ff5e32569
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-12-06 00:51:19 +0600

    update for prod

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function start() {
   //Set CORS options
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://jarvis-gpt-v1.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   };
